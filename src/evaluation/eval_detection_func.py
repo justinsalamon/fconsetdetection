34468f91ca671d8098881333dc38fd3dbaf2b194
Justin Salamon <justin.salamon@gmail.com>
2015-09-24 11:10:44 -0400

    fast peak picking, all lines <=80 char

--- src/evaluation/eval_detection_func.py
+++ src/evaluation/eval_detection_func.py
@@ -4,11 +4,13 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 
-def eval_detection_func(annotation_path, function_path, start_time, dt, limit=3000):
+def eval_detection_func(annotation_path, function_path, start_time, dt,
+                        limit=3000):
     """
-        Evaluates a detection function. Given the output of a detection function we find
-    the peaks, and compare them to a list of ground-truth annotations. Then print out the
-    maximum F-measure found and display a precision-recall graph.
+    Evaluates a detection function. Given the output of a detection function we
+    find the peaks, and compare them to a list of ground-truth annotations.
+    Then print out the maximum F-measure found and display a precision-recall
+    graph.
     :param annotation_path: String
     :param function_path: String
     :param start_time: float
@@ -23,7 +25,8 @@ def eval_detection_func(annotation_path, function_path, start_time, dt, limit=30
     detection_function = np.load(function_path)[:, 0]
 
     # Get reference onsets for ground truth
-    df = pd.read_csv(annotation_path, header=None, names=['onsets', 'offsets', 'label'], delimiter='\t')
+    df = pd.read_csv(annotation_path, header=None,
+                     names=['onsets', 'offsets', 'label'], delimiter='\t')
     ref_onsets = np.asarray(df['onsets'])
 
     # Get list of precisions and recalls for varying thresholds
@@ -33,7 +36,8 @@ def eval_detection_func(annotation_path, function_path, start_time, dt, limit=30
     max_F = 0
 
     for threshold in np.linspace(0, 1):
-        est_onsets = pick_peaks(detection_function, threshold, start_time, dt, t_end)
+        est_onsets_ind = pick_peaks(detection_function, threshold)
+        est_onsets = indices_to_times(est_onsets_ind, start_time, dt)
         F, P, R = mir_eval.onset.f_measure(ref_onsets,
                                            est_onsets,
                                            window=0.2)
@@ -56,16 +60,23 @@ def eval_detection_func(annotation_path, function_path, start_time, dt, limit=30
     plt.show()
 
 
-def pick_peaks(detection_function, threshold, start_time, dt, t_end):
+def pick_peaks(detection_function, threshold):
     """
     Given the output of a detection function, finds the indices of the peaks
     :param detection_function: numpy array
     :param threshold: float
     :return: numpy array of ints
     """
-    i_end = times_to_indices(t_end, start_time, dt)
-    detection_function = detection_function[:i_end]
-    mask = np.ma.greater[detection_function>threshold]
+    # i_end = times_to_indices(t_end, start_time, dt)
+    # detection_function = detection_function[:i_end]
+    # mask = np.ma.greater[detection_function>threshold]
+
+    smaller = detection_function[0:-2] <= detection_function[1:-1]
+    larger = detection_function[1:-1] > detection_function[2:]
+    over_threshold = detection_function[1:-1] >= threshold
+    peaks = smaller*larger*over_threshold
+    locs = np.where(peaks==True)[0] + 1
+    return locs
 
 
 def times_to_indices(times, start_time, dt):
@@ -75,6 +86,9 @@ def times_to_indices(times, start_time, dt):
 def indices_to_times(indices, start_time, dt):
     return start_time + indices * dt
 
-eval_detection_func("../annotations/NSDNS_20110902_192900_high_and_low.txt",
-                    "../detection_functions/NSDNS_20110902_192900_streaming_prob.npy",
-                    0.07619047619, 0.05079365079)
+
+path_ref = "../annotations/NSDNS_20110902_192900_high_and_low.txt"
+path_est = "../detection_functions/NSDNS_20110902_192900_streaming_prob.npy"
+dt = 0.05079365079
+start_time = 0.07619047619
+eval_detection_func(path_ref, path_est, start_time, dt)
